ff83945996e7ae3029dfce2e2335fda91ede514d
Carver Harrison <pcarverb@hotmail.com>
2016-07-29 18:28:54 -0700

    dankmeme 0.0.0.0.1

--- leetnet/Browser.cs
+++ leetnet/Browser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -26,7 +27,35 @@ namespace leetnet
 
         public void LoadPage(string text)
         {
-            Socket s = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                ASCIIEncoding asen = new ASCIIEncoding();
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                string ip = text.Split('/')[0];
+                string dank = "";
+                tcpclnt.Connect(ip, 13370);
+                Console.WriteLine("Connected");
+                Stream stm = tcpclnt.GetStream();
+                string header = $"[page={text};version=1.0;]";
+                stm.Write(asen.GetBytes(header), 0, header.Length);
+                
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                     dank += Convert.ToChar(bb[i]).ToString();
+                webBrowser1.DocumentText = dank;
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("FUCKING BUBBLES: " + e.StackTrace);
+            }
         }
     }
 }
