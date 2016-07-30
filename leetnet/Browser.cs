c0d44b9a28e34ec471f930e2b3e4b05ef564e317
Carver Harrison <pcarverb@hotmail.com>
2016-07-29 18:59:24 -0700

    0.0.2

--- leetnet/Browser.cs
+++ leetnet/Browser.cs
@@ -38,18 +38,54 @@ namespace leetnet
                 tcpclnt.Connect(ip, 13370);
                 Console.WriteLine("Connected");
                 Stream stm = tcpclnt.GetStream();
-                string header = $"[page={text};version=1.0;]";
-                stm.Write(asen.GetBytes(header), 0, header.Length);
-                
-
                 byte[] bb = new byte[100];
                 int k = stm.Read(bb, 0, 100);
+                
+                switch (Convert.ToChar(bb[0]).ToString())
+                {
+                    case "100":
+                        // 100 OKAY
+                        for (int i = 1; i < k; i++)
+                            dank += Convert.ToChar(bb[i]).ToString();
+                        webBrowser1.DocumentText = dank;
 
-                for (int i = 0; i < k; i++)
-                     dank += Convert.ToChar(bb[i]).ToString();
-                webBrowser1.DocumentText = dank;
+                        tcpclnt.Close();
+                        break;
+                    case "101":
+                        // 101 REDIRECT
+                        break;
+                    case "102":
+                        // 102 BROWSER SPECIFIC
+                        break;
+                    case "200":
+                        // 200 FILE NOT FOUND
+                        webBrowser1.DocumentText = "<H1>ERROR 200</H1><P>File Not Found</P>";
+                        break;
+                    case "201":
+                        // 201 GONE
+                        webBrowser1.DocumentText = "<H1>ERROR 201</H1><P>Gone</P>";
+                        break;
+                    case "202":
+                        // 202 GENERIC
+                        webBrowser1.DocumentText = "<H1>ERROR 202</H1><P>Generic<P>";
+                        break;
+                    case "300":
+                        // 300 SERVER DOWN
+                        webBrowser1.DocumentText = "<H1>ERROR 300</H1><P>Server Down</P>";
+                        break;
+                    case "301":
+                        // 301 SERVER ERROR
+                        webBrowser1.DocumentText = "<H1>ERROR 301</H1><P>Server Error</P>";
+                        break;
+                    default:
+                        // 100 OKAY
+                        for (int i = 1; i < k; i++)
+                            dank += Convert.ToChar(bb[i]).ToString();
+                        webBrowser1.DocumentText = dank;
 
-                tcpclnt.Close();
+                        tcpclnt.Close();
+                        break;
+                }
             }
 
             catch (Exception e)
