b47e898a421eedffd09e7b78c119f4a03737455a
Carver Harrison <pcarverb@hotmail.com>
2016-07-31 14:11:06 -0700

    0.2

--- servertest/Program.cs
+++ servertest/Program.cs
@@ -13,38 +13,41 @@ namespace servertest
     {
         public static void Main()
         {
-            try
-            {
-                IPAddress ipAd = IPAddress.Parse("127.0.0.1");
-                // use local m/c IP address, and 
-                // use the same in the client
+			while (true)
+			{
+				try
+				{
+					IPAddress ipAd = IPAddress.Parse("10.0.0.5");
+					// use local m/c IP address, and 
+					// use the same in the client
 
-                /* Initializes the Listener */
-                TcpListener myList = new TcpListener(ipAd, 13370);
+					/* Initializes the Listener */
+					TcpListener myList = new TcpListener(ipAd, 13370);
 
-                /* Start Listeneting at the specified port */
-                myList.Start();
+					/* Start Listeneting at the specified port */
+					myList.Start();
 
-                Console.WriteLine("The local End point is  :" +
-                                  myList.LocalEndpoint);
-                Console.WriteLine("Waiting for a connection.....");
+					Console.WriteLine("The local End point is  :" +
+									  myList.LocalEndpoint);
+					Console.WriteLine("Waiting for a connection.....");
 
-                Socket s = myList.AcceptSocket();
-                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-                ASCIIEncoding asen = new ASCIIEncoding();
+					Socket s = myList.AcceptSocket();
+					Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+					ASCIIEncoding asen = new ASCIIEncoding();
 
-                s.Send(File.ReadAllBytes("default.md"));
-                s.Send(new byte[] { 4 });
+					s.Send(File.ReadAllBytes("default.md"));
+					s.Send(new byte[] { 4 });
 
-                /* clean up */
-                s.Close();
-                myList.Stop();
+					/* clean up */
+					s.Close();
+					myList.Stop();
 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error..... " + e.StackTrace);
-            }
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Error..... " + e.StackTrace);
+				}
+			}
         }
     }
 }
